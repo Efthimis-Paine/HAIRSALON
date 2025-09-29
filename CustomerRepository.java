2f123445b3b02a519e7ff0ce7a72593277e8243e
Efthimis-Paine <painesise@gmail.com>
2025-09-29 16:24:08 +0300

    Delete CustomerRepository.java

--- CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hairsalon.repository;
-
-import com.hairsalon.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
